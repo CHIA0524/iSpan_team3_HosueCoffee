850cc35f017890d9cbe42eb08c2a89d31570026f
meowen0228 <meowen0228@gmail.com>
2022-05-09 10:12:21 +0800

    Wen icon SVG檔 製作成Component

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import LeftArrowIcon from './component/icon/LeftArrowIcon'
+import RightArrowIcon from './component/icon/RightArrowIcon'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <LeftArrowIcon />
+        <RightArrowIcon />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +23,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
