d285d9a2547c0c263b7c22ade4f4c203b9d50ac3
9an <101699368+thisis9ann@users.noreply.github.com>
2022-05-14 14:43:30 +0800

    5/14 -1

---
+++ layout/js/home.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".appeal").mouseover(function(){
+      $(".appeal").css("size","110%");
+    });
+    $(".appeal").mouseout(function(){
+      $(".appeal").css("size","100%");
+    });
+  });
