4c1551d5e5853dfbbece7f15655058d80775e58a
9an <101699368+thisis9ann@users.noreply.github.com>
2022-05-19 13:58:11 +0800

    5/19 -1

--- layout/js/home.js
+++ layout/js/home.js
@@ -1,8 +1,15 @@
-$(document).ready(function(){
-    $(".appeal").mouseover(function(){
-      $(".appeal").css("size","110%");
-    });
-    $(".appeal").mouseout(function(){
-      $(".appeal").css("size","100%");
-    });
-  });
+$(window).scroll(function() {
+  var h = $(window).scrollTop();  //取得捲動的距離(與top的距離)
+  if (h > 100) {
+    $('.topBTN:hover').css('display', 'block');
+  }  //如果距離大於100時 將ui-gotop這個改為 display: block
+  else {
+    $('.topBTN:hover').css('display', 'none');
+  }  //除此之外，也就是小於100時，display: none
+})
+$('.topBTN:hover').click(function() {
+  //點擊ui-gotop之後
+  $('html,body').animate({
+    scrollTop: 0
+  }, 'fast');  //回到0的位置
+})
