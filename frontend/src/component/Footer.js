f644e07f58fc707af0105f9a0781db659aeb7cdd
meowen0228 <meowen0228@gmail.com>
2022-05-19 21:39:18 +0800

    Wen navbar footer 拆出成組件

---
+++ frontend/src/component/Footer.js
@@ -0,0 +1,46 @@
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import React from 'react';
+import './Footer.scss'
+
+// icons
+import { IoCallOutline } from "react-icons/io5";
+import { IoMailOutline } from "react-icons/io5";
+import { BiCopyright } from "react-icons/bi";
+
+function Footer (){
+
+  return(
+    <footer className="d-flex coffeeFooter">
+      <div className="d-flex">
+        <div className="mx-5">
+          PRODUCT
+          <li><a href="">coffee</a></li>
+          <li><a href="">cups</a></li>
+          <li><a href="">other</a></li>
+        </div>
+        <div className="mx-5">
+          ABOUT US
+          <li><a href="">news</a></li>
+          <li><a href="">store</a></li>
+          <li><a href="">FAQs</a></li>
+        </div>
+      </div>
+      <div className="mx-5">
+        <div className="d-flex">
+          <IoMailOutline size={20}/>
+          <p>team3@house.coffee.com</p>
+        </div>
+        <div className="d-flex">
+          <IoCallOutline size={20}/>
+          <p>(07)333-3333</p>
+        </div>
+        <div className="d-flex">
+          <BiCopyright size={20}/>
+          <p>2022 . HOUSE COFFEE</p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
