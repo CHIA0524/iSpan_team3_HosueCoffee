2fdfda36ffb51d25e1e788bfb09596db18f9c04c
chu <101313532+marsting@users.noreply.github.com>
2022-05-26 11:49:47 +0800

    pop

--- frontend/src/pages/shop/ShopIndex.js
+++ frontend/src/pages/shop/ShopIndex.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch ,useParams} from 'react-router-dom';
 import './brandproduct.css';
 import './component/popup.css';
 import ProductCard from './component/ProductCard';
@@ -8,6 +8,32 @@ import Aside from './component/Aside';
 
 function ShopIndex(){
     const[amount, setAmount]= useState(1)
+
+    const params=useParams();
+    //抓網址後的id
+    console.log(params)
+      // 向後端請求資料
+    const [datas, setDatas ] = useState([])
+    const fetchData = async()=>{
+      const response = await fetch(`http://localhost:3001/shop#id?id=${params.id}`);
+      const results = await response.json();
+      setDatas(results);
+    }
+    useEffect(()=>{
+      fetchData();
+    },[])
+        
+    if(datas.length>0){
+      console.log(datas[0]);
+      const PD=datas[0];
+      const{id,p_name,price,content}=PD
+    //   console.log(p_name);
+    
+    const img1=(p_name);
+    // const id =(id);
+    console.log(id);
+  
+    
 
   return(
     <>
@@ -26,20 +52,19 @@ function ShopIndex(){
                    
                 </div>
                 {/* <!-- popup 區 --> */}
-                <div id="popup1" class="popupShow">
+                <div id={id} class="popupShow">
                     <div class="popup">
                         <a class="close" href="#">&times;</a>
                         <div class="productPopup">
                             <div class="popBox">
                                 <div class="popBoxContent">
                                     <div class="imgPart">
-                                        <img class="packageImg" src={require('./img/包裝 2.png')} alt=""></img>
+                                        <img class="packageImg" src={require('./img/'+img1+'.jpg')} alt=""></img>
                                     </div>
                                     <div class="popContent">
                                         <div class="popText">
-                                            <p>伊莎米 </p>
-                                            <p>精選招牌特調(半磅)</p>
-                                            <p>$499</p>
+                                            <p>{p_name} </p>
+                                            <p>${price}</p>
                                         </div>
                                         <div class="popBtn">
                                             <div class="popAddNum">
@@ -65,6 +90,7 @@ function ShopIndex(){
     </div>
     </>
   );
+}
 }
 
 export default ShopIndex
