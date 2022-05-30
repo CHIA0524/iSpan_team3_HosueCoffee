6718ad4504e91b942a30acc45d7c469181365ebf
meowen0228 <meowen0228@gmail.com>
2022-05-30 14:23:27 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,25 @@
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
-import './App.scss'
-
-// components
-import Navbar from './component/Navbar'
-import Footer from './component/Footer'
-
-// home pages
-import Home from './pages/home/Home'
-
-// store pages
-import StoreMapCardWrap from './pages/store/StoreMapCardWrap'
-
-// member pages
-// import MemberLogin from './pages/member/memberlogin'
-// import Memberprofile from './pages/member/memberprofile'
-// import MemberprofileEdit from './pages/member/memberprofileEdit'
-// import MemberPassword from './pages/member/memberPassword'
-// import MemberOrderList from './pages/member/memberOrderList'
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-
-      <Navbar/>
-
-
-      {/* Switch 只有此範圍會換畫面 navbar footer 會保留 */}
-      {/* 記得載入頁面組件喔 */}
-      {/* 路徑長的往上放喔 */}
-
-      <Switch>
-
-        <Route path="/store"><StoreMapCardWrap /></Route>
-        
-        <Route path="/" exact><Home /></Route>
-
-      </Switch>
-
-      <Footer/>
-
-    </Router>
-
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
