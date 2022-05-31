0e7cd3826511784945cc360bec75ddbd69bdb2f6
Meowen <meowen0228@gmail.com>
2022-05-31 12:34:50 +0800

    Wen navbar 修改

--- frontend/src/component/Navbar.js
+++ frontend/src/component/Navbar.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch, useLocation } from 'react-router-dom';
+import { useState, useEffect, useRef } from 'react'
 import React from 'react';
 import logo from './img/logo.svg';
 import './Navbar.scss'
@@ -10,24 +11,19 @@ import { IoExitOutline } from "react-icons/io5";
 import { IoMenuOutline } from "react-icons/io5";
 import { IoCartOutline } from "react-icons/io5";
 
-function Navbar (props){
-  const{auth}=props;
+function Navbar (){
 
-  const loginOUT=()=>{
-    localStorage.clear();
-    alert("您已登出")
-    window.location.assign("http://localhost:3000/");
-
+  let homeCSS ;
+  let getURL = useLocation();
+  console.log(getURL);
+  if(getURL.pathname==='/'){
+    homeCSS = {background: 'transparent', boxShadow: 'none', position: 'fixed', marginButtom: '0', top: '0'}
   }
-  
-  // const thismemberid=localStorage.getItem(true);
-  // console.log("這是bar");
-  // console.log(thismemberid);
-  // console.log("這是bar");
 
   // 手機板 navbar js
   const openSideNavClick = ()=>{
     document.querySelector('.navFirst').style.left = '0px';
+    document.querySelector('.navFirst').style.transition = 'all .3s ease-in-out';
     document.querySelector('.sideDark').style.display = 'block';
   }
   const closeSideNavClick = ()=>{
@@ -44,115 +40,69 @@ function Navbar (props){
     }
   }
 
+  // 監測視窗寬度
+  useEffect(()=>{ 
+    window.addEventListener('resize',()=>{
+      // console.log(window.innerWidth);
+      if (window.innerWidth >= 1000) {
+        document.querySelector('.navFirst').style.transition = '';
+        document.querySelector('.sideDark').style.display = 'none';
+      }
+    });
+  },[]);
+
   return(
-    <>
-    {auth?   <header className="App-header">
-        <nav className="coffeeNavbar">
-          <div className="sideDark" onClick={closeSideNavClick}></div>
-          <div className="navFirst" style={{ left: '-260px' }}>
-            <li className="closeSideNav webNone" onClick={closeSideNavClick}>
-              <IoCloseOutline size={30}/>
-            </li>
-            <li className="webNone">
-              <Link to="/member/Profile">
-                <IoPersonOutline size={20} color={'#4C3410'}/>
-              </Link>
-              <a className="" href="#">
-                <p className="openMemberDetail" onClick={openMemberDetailClick}>MEMBER</p>
-              </a>
-              <a className="ms-3" href="#" onClick={loginOUT}>
-                <IoExitOutline size={20} color={'#4C3410'}/>
-              </a>
-            </li>
-            <div className="memberDetail" style={{height: '0px'}}>
-              {/* <li><Link to="/member/OrderList">個人資料</Link></li> */}
-              <li><Link to="/member/OrderList">訂單查詢</Link></li>
-              <li><Link to="/member/DrinkOList">點餐訂單</Link></li>
-              <li><Link to="/member/QAList">問答中心</Link></li>
-              <li><Link to="/member/Password">密碼修改</Link></li>
-              <li><Link to="/member/Favorite">我的收藏</Link></li>
-            </div>
-            <li><a href="#" onClick={closeSideNavClick}>HOME<span>首頁</span></a></li>
-            <li><a href="#" onClick={closeSideNavClick}>SHOP<span>網路商城</span></a></li>
-            <li><a href="#" onClick={closeSideNavClick}>BLOG<span>部落格</span></a></li>
-            <li><Link to="/store" onClick={closeSideNavClick}>STORE<span>門市地圖</span></Link></li>
-            <li className="webNone"><a href="#"><button className="coffeeLightBtn">{">> Order Online"}</button></a></li>
+    <header className="App-header">
+      <nav className="coffeeNavbar" style={homeCSS}>
+        <div className="sideDark" onClick={closeSideNavClick}></div>
+        <div className="navFirst" style={{ left: '-260px' }}>
+          <li className="closeSideNav webNone" onClick={closeSideNavClick}>
+            <IoCloseOutline size={30}/>
+          </li>
+          <li className="webNone">
+            <a className="" href="#">
+              <IoPersonOutline size={20} color={'#4C3410'}/>
+            </a>
+              <p className="openMemberDetail" onClick={openMemberDetailClick}>MEMBER</p>
+            <a className="ms-3" href="#">
+              <IoExitOutline size={20} color={'#4C3410'}/>
+            </a>
+          </li>
+          <div className="memberDetail" style={{height: '0px'}}>
+            <li><a href="" onClick={closeSideNavClick}>訂單查詢</a></li>
+            <li><a href="" onClick={closeSideNavClick}>點餐訂單</a></li>
+            <li><a href="" onClick={closeSideNavClick}>問答中心</a></li>
+            <li><a href="" onClick={closeSideNavClick}>資料維護</a></li>
+            <li><a href="" onClick={closeSideNavClick}>密碼修改</a></li>
           </div>
-          <div className="openSideNav" style={{cursor: 'pointer'}} onClick={openSideNavClick}>
-            <IoMenuOutline size={30}/>
-          </div>
-          <div>
-            <li className="logo"><Link to="/"><img src={logo} alt="logo"></img></Link></li>
-          </div>
-          <div>
-            <li className="mobileNone"><a href="#"><button className="coffeeLightBtn">{">> Order Online"}</button></a></li>
-            <li><a><IoCartOutline size={30}/></a></li>
+          <li><a href="#" onClick={closeSideNavClick}>HOME<span>首頁</span></a></li>
+          <li><a href="#" onClick={closeSideNavClick}>SHOP<span>網路商城</span></a></li>
+          <li><a href="#" onClick={closeSideNavClick}>BLOG<span>部落格</span></a></li>
+          <li><Link to="/store" onClick={closeSideNavClick}>STORE<span>門市地圖</span></Link></li>
+          <li className="webNone"><a href="#"><button className="coffeeLightBtn">{">> Order Online"}</button></a></li>
+        </div>
+        <div className="openSideNav" onClick={openSideNavClick}>
+          <IoMenuOutline size={30}/>
+        </div>
+        <div>
+          <li className="logo"><Link to="/"><img src={logo} alt="logo"></img></Link></li>
+        </div>
+        <div>
+          <li className="mobileNone"><a href="#"><button className="coffeeLightBtn">{">> Order Online"}</button></a></li>
+          <li><a><IoCartOutline size={30}/></a></li>
 
-            <li className="mobileNone position-relative">
-              <Link to="/member"><IoPersonOutline size={30}/></Link>
-              <p className="webUserInfo">
-                <span><Link to="/member">會員專區</Link></span>
-                <span><Link to="OrderList">訂單查詢</Link></span>
-                <span><Link to="/member/QAList">問答中心</Link></span>
-                <span><a onClick={loginOUT}>登出</a></span>
-              </p>
-            </li>
-          </div>
-        </nav>
-      </header>
-      /* 以上是已登入狀態 */
-      :
-      /* 以下是未登入狀態 */
-      <header className="App-header">
-        <nav className="coffeeNavbar">
-          <div className="sideDark" onClick={closeSideNavClick}></div>
-          <div className="navFirst" style={{ left: '-260px' }}>
-            <li className="closeSideNav webNone" onClick={closeSideNavClick}>
-              <IoCloseOutline size={30}/>
-            </li>
-            <li className="webNone">
-              <Link to="/member">
-                <IoPersonOutline size={20} color={'#4C3410'}/>
-              </Link>
-              <Link to="/member">
-                <p className="openMemberDetail" onClick={openMemberDetailClick}>MEMBER</p>
-                </Link>
-              {/* <a className="ms-3" href="#">
-                <IoExitOutline size={20} color={'#4C3410'}/>
-              </a> */}
-            </li>
-            {/* <div className="memberDetail" style={{height: '0px'}}>
-              <li><Link to="/member/OrderList">訂單查詢</Link></li>
-              <li><Link to="/member/DrinkOList">點餐訂單</Link></li>
-              <li><Link to="/member/QAList">問答中心</Link></li>
-              <li><Link to="/member/Password">密碼修改</Link></li>
-              <li><Link to="/member/Favorite">我的收藏</Link></li>
-            </div> */}
-            <li><a href="#" onClick={closeSideNavClick}>HOME<span>首頁</span></a></li>
-            <li><a href="#" onClick={closeSideNavClick}>SHOP<span>網路商城</span></a></li>
-            <li><a href="#" onClick={closeSideNavClick}>BLOG<span>部落格</span></a></li>
-            <li><Link to="/store" onClick={closeSideNavClick}>STORE<span>門市地圖</span></Link></li>
-            <li className="webNone"><a href="#"><button className="coffeeLightBtn">{">> Order Online"}</button></a></li>
-          </div>
-          <div className="openSideNav" style={{cursor: 'pointer'}} onClick={openSideNavClick}>
-            <IoMenuOutline size={30}/>
-          </div>
-          <div>
-            <li className="logo"><Link to="/"><img src={logo} alt="logo"></img></Link></li>
-          </div>
-          <div>
-            <li className="mobileNone"><a href="#"><button className="coffeeLightBtn">{">> Order Online"}</button></a></li>
-            <li><a><IoCartOutline size={30}/></a></li>
-
-            <li className="mobileNone position-relative">
-              <Link to="/member"><IoPersonOutline size={30}/></Link>
-            
-            </li>
-          </div>
-        </nav>
-      </header>}
-    
-      </>
+          <li className="mobileNone position-relative">
+            <Link to="/memberLogin"><IoPersonOutline size={30}/></Link>
+            <p className="webUserInfo">
+              <span><a href="">會員專區</a></span>
+              <span><a href="">訂單查詢</a></span>
+              <span><a href="">問答中心</a></span>
+              <span><a href="">登出</a></span>
+            </p>
+          </li>
+        </div>
+      </nav>
+    </header>
   )
 }
 
