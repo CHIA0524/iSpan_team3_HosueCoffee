8bd74b4c80443828e8de7d45e9b25e8b137f08b5
meowen0228 <meowen0228@gmail.com>
2022-06-01 16:49:53 +0800

    Wen 篩選寫到一半
    樣式還未完成
    還沒接資料

--- frontend/src/pages/store/StoreCardSearch.js
+++ frontend/src/pages/store/StoreCardSearch.js
@@ -3,16 +3,52 @@ import { useState, useEffect } from 'react'
 
 // icons
 import { FiSearch } from "react-icons/fi"
+import { IoOptionsOutline } from "react-icons/io5"
 
 function StoreCardSearch(props){
 
   const { setIsLoading, fetchFilterData } = props
   const [ searchText, setSearchText ] = useState('')
-
+  const [ filterCSS, setFilterCSS ] = useState(false)
+  const DOW = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日']
   return(
     <div className="storeSearch">
+
+      {/*篩選清單開關*/}
+      <div onClick={(e)=>{
+        if(filterCSS){
+          setFilterCSS(false)
+        }else{
+          setFilterCSS(true)
+        }
+      }}>
+        <IoOptionsOutline size={25}/>
+      </div>
+
+      {/*篩選清單*/}
+      <div className="storeFilter" style={{display: 'flex'}}>
+        <div>
+          <p>營業時間</p>
+          {DOW.map((v, i)=>{
+            return(
+              <li key={i}>
+                <label>
+                  <input type="checkbox"></input>
+                  {v}
+                </label>
+              </li>
+            )
+          })}
+        </div>
+        <div>
+          <p>服務項目</p>
+        </div>
+
+      </div>
+
+      {/*查詢框*/}
       <input
-        className="store-search-input"
+        className="storeSearchInput"
         name="search-for"
         placeholder="搜尋門市名稱或地址"
         onChange={(e)=>{
@@ -24,6 +60,7 @@ function StoreCardSearch(props){
       >
       </input>
 
+      {/*搜尋按鈕*/}
       <div onClick={(e)=>{
         console.log(searchText)
         // setSearchText('')
@@ -32,6 +69,7 @@ function StoreCardSearch(props){
       }}>
         <FiSearch size={20}/>
       </div>
+
     </div>
   )
 }
