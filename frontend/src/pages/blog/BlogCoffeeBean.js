1976d055c114f0d12f89e997deea44b98c4dfa36
CHIA0524 <love860524boy@gmail.com>
2022-06-15 23:30:22 +0800

    blog RWD select

--- frontend/src/pages/blog/BlogCoffeeBean.js
+++ frontend/src/pages/blog/BlogCoffeeBean.js
@@ -28,6 +28,27 @@ function BlogCoffeeBean(props){
     
         
     }
+    const find = (event) => {
+      event.preventDefault();
+      if (event.target.value === "coffee") {
+          window.location.assign('/blog/coffee')
+          }
+           else if (event.target.value === "coffeebean") {
+           window.location.assign('/blog/coffeebean')
+           }
+           else if (event.target.value === "pourover") {
+              window.location.assign('/blog/pourover')
+              }
+          else if (event.target.value === "celebrity") {
+              window.location.assign('/blog/celebrity')
+              }
+          else if (event.target.value =="share") {
+              window.location.assign('/blog/share')
+              }
+          else{
+              window.location.assign('/blog')
+          }
+      }
   
       // didMount
       useEffect(() => {
@@ -56,7 +77,20 @@ function BlogCoffeeBean(props){
 
        {/* //行動裝置板選單 */}
         <div className="blogType container">
-          <BlogMobNav />
+        <select
+            className="form-select blogSelect "
+            name="article"
+            id="article"
+            onChange={(e) => find(e)}
+            >
+            <option value="" >全部文章</option> 
+            <option value="coffee">咖啡篇</option>
+            <option value="coffeebean" selected>咖啡豆篇</option>
+            <option value="pourover">沖煮篇</option>
+            <option value="celebrity">名人專欄篇</option>
+            <option value="share">好物分享</option>
+               
+            </select>
         </div>
 
 
--- frontend/src/pages/blog/BlogShare.js
+++ frontend/src/pages/blog/BlogShare.js
@@ -30,6 +30,27 @@ function BlogShare(props){
     
         
     }
+    const find = (event) => {
+      event.preventDefault();
+      if (event.target.value === "coffee") {
+          window.location.assign('/blog/coffee')
+          }
+           else if (event.target.value === "coffeebean") {
+           window.location.assign('/blog/coffeebean')
+           }
+           else if (event.target.value === "pourover") {
+              window.location.assign('/blog/pourover')
+              }
+          else if (event.target.value === "celebrity") {
+              window.location.assign('/blog/celebrity')
+              }
+          else if (event.target.value =="share") {
+              window.location.assign('/blog/share')
+              }
+          else{
+              window.location.assign('/blog')
+          }
+      }
     //console.log(blog);
     
       // didMount
@@ -59,7 +80,20 @@ function BlogShare(props){
 
          {/* //行動裝置板選單 */}
          <div className="blogType container">
-          <BlogMobNav />
+         <select
+            className="form-select blogSelect "
+            name="article"
+            id="article"
+            onChange={(e) => find(e)}
+            >
+            <option value="" >全部文章</option> 
+            <option value="coffee">咖啡篇</option>
+            <option value="coffeebean">咖啡豆篇</option>
+            <option value="pourover">沖煮篇</option>
+            <option value="celebrity">名人專欄篇</option>
+            <option value="share" selected>好物分享</option>
+               
+            </select>
         </div>
 
 
