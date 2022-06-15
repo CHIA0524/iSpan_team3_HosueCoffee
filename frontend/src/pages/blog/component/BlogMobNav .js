9e37cad89bd508dbebec3941a968b00a15ad7112
JasonMing666 <crazy.Ming666@gmail.com>
2022-06-15 18:59:27 +0800

    blog-要調整 img和文章select

--- frontend/src/pages/blog/component/BlogMobNav .js
+++ frontend/src/pages/blog/component/BlogMobNav .js
@@ -9,28 +9,49 @@ import '../blogBanner.css';
 
 
 const BlogMobNav = (props) => {
- 
+    const nowWeb=window.location.pathname
+    console.log(window.location.pathname); 
+    console.log(nowWeb.slice(6,)); 
+    const nowpath=nowWeb.slice(6,)
     // 行動裝置板選單
 //  const [selectedValue, setSelectedValue] = useState('')
  const articleOptions = [{name:"咖啡篇",value:"coffee"}, {name:"咖啡豆篇",value:"coffeebean"}, {name:"沖煮篇",value:"pourover"}, {name:"名人專欄篇",value:"celebrity"}, {name:"好物分享篇",value:"share"}]   
+    const find = (event) => {
+        event.preventDefault();
+        if (event.target.value === "coffee") {
+            window.location.assign('/blog/coffee')
+            }
+            else if (event.target.value === "coffeebean") {
+            window.location.assign('/blog/coffeebean')
+            }
+            else if (event.target.value === "pourover") {
+                window.location.assign('/blog/pourover')
+                }
+            else if (event.target.value === "celebrity") {
+                window.location.assign('/blog/celebrity')
+                }
+            else {
+                window.location.assign('/blog/share')
+            }
+        }
 
-
-    return (
-        <>
+return (
+    <>
             {/* //行動裝置板選單  */}
          <select
             className="form-select blogSelect "
             name="article"
             id="article"
-            onChange={(e) => {
-                const selectgoto=document.getElementById("article").value
-                window.location.assign('/blog/'+selectgoto)
+            onChange={(e) => find(e)} 
+                // const selectgoto=document.getElementById("article").value
+                // window.location.assign('/blog/'+selectgoto)
             
-              }
+              
                 
-            }
+            // }
 
             >
+   
             <option value="">文章類別</option>
             {articleOptions.map((v, i) => {
                     console.log(v.value)
