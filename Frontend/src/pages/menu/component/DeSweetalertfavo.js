54a7442b75b63bcffd3fac91b1868bea0885e3f6
Meowen <meowen0228@gmail.com>
2022-07-06 23:08:01 +0800

    Revert "刪除錯誤的資料夾"
    This reverts commit 37b219eb58829e0d0377537c4672dc1506ca764c.

---
+++ Frontend/src/pages/menu/component/DeSweetalertfavo.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+
+
+
+const DeSweetalertfavo = () =>{
+
+const MySwal = withReactContent(Swal)
+
+MySwal.fire({
+    title: <p>已取消收藏</p>,
+    icon: 'success',
+    showConfirmButton: false,
+    timer: 1300,
+    })
+}
+
+
+export default DeSweetalertfavo
